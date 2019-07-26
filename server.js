9ae8ee6e726a607174c5a3b9185f600a32b7b6b7
Mari0203 <mari.brands@pm.me>
2019-07-25 23:49:10 -0500

    Initial commit w/ directory and file setup + Configurations in server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+// ========== DEPENDENCIES ========== //
+var express = require("express");
+var path = require("path");
+
+// ========== EXPRESS CONFIGURATION ========== //
+var app = express();
+
+// ========== PORTS SETUP ========== //
+var PORT = process.env.PORT || 7000;
+
+// ========== SETS UP EXPRESS APP TO HANDLE DATA PARSING ========== //
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ========== ROUTES ========== //
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// ========== LISTENER (STARTS THE SERVER) ========== //
+app.listen(PORT, function() {
+    console.log("App listening on PORT: ", PORT);
+});

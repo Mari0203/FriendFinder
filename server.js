7e8882f6d3b82df094487f9324936769b2b71083
Mari0203 <mari.brands@pm.me>
2019-10-03 15:06:13 -0500

    Added home.html with jumbotron, button and Users API link

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ var express = require("express");
 var path = require("path");
 
 // ========== EXPRESS CONFIGURATION ========== //
+// Instantiate Express server
 var app = express();
 
 // ========== PORTS SETUP ========== //
 var PORT = process.env.PORT || 7000;
 
-// ========== SETS UP EXPRESS APP TO HANDLE DATA PARSING ========== //
+// ========== SET UP EXPRESS APP TO HANDLE DATA PARSING ========== //
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 

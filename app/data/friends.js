92f5b3d30ea5cd410b246bc8caf81ba260554c02
Mari0203 <mari.brands@pm.me>
2019-10-03 23:28:20 -0500

    Uploaded images and url links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,59 @@
 var friends = [
-    {
-    "name":"Gustav",
-    "photo":"http://www.studiotreasure.com/artists/k/Klimt-images/Gustav-Klimt.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-        ]
-    },
-    {
-        
-    }
+  {
+    name: "Gustav Klimt",
+    photo:
+      "http://www.studiotreasure.com/artists/k/Klimt-images/Gustav-Klimt.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Ludwig van Beethoven",
+    photo:
+      "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjXxsbJ24HlAhXPna0KHYTeBwMQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.biography.com%2Fmusician%2Fludwig-van-beethoven&psig=AOvVaw0dQUGTvXhR47QtYwh4uufK&ust=1570247940946384",
+    scores: [7, 2, 4, 5, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Mohandas Gandhi",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/f/f3/Mohandas_K._Gandhi%2C_portrait.jpg",
+    scores: [1, 2, 5, 5, 5, 3, 2, 5, 4, 4]
+  },
+  {
+    name: "Grace Hopper",
+    photo: "https://edittress.com/wp-content/uploads/2018/02/grace-hopper.jpg",
+    scores: [1, 3, 1, 1, 2, 3, 2, 5, 4, 4]
+  },
+  {
+    name: "Tesla Nikola",
+    photo:
+      "https://images-na.ssl-images-amazon.com/images/I/71QFbhNzbjL._SL1000_.jpg",
+    scores: [4, 1, 3, 2, 2, 3, 2, 5, 5, 4]
+  },
+  {
+    name: "Frida Kahlo",
+    photo:
+      "http://ksassets.timeincuk.net/wp/uploads/sites/46/2016/07/Frida-Kahlo-1.jpg",
+    scores: [4, 1, 3, 2, 2, 3, 2, 5, 5, 4]
+  },
+  {
+    name: "Tupac Shakur",
+    photo:
+      "https://www.rockhall.com/sites/default/files/styles/header_image_portrait/public/tupac-crop.jpg?itok=M8XT2jaK",
+    scores: [1, 1, 4, 5, 2, 3, 2, 3, 5, 4]
+  },
+  {
+    name: "Albert Einstein",
+    photo:
+      "https://www.biography.com/.image/t_share/MTE5NTU2MzE2MjExMDg2ODU5/albert-einstein-sticking-out-his-tongue.jpg",
+    scores: [1, 1, 4, 5, 2, 3, 2, 3, 5, 4]
+  },
+  {
+    name: "Salvador Dali",
+    photo: "https://images-na.ssl-images-amazon.com/images/I/51soAG8G0tL.jpg",
+    scores: [1, 5, 4, 3, 2, 2, 2, 3, 1, 4]
+  },
+  {
+    name: "Dr. Evil",
+    photo: "https://miro.medium.com/max/1252/1*ZYpBSAe0dC4_ha-3GhcO9Q.jpeg",
+    scores: [1, 5, 4, 3, 2, 2, 2, 3, 1, 4]
+  }
 ];

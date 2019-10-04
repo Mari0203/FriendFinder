76d9cdd300e0aa9eae8ac7f3fcb4faee08894051
Mari0203 <mari.brands@pm.me>
2019-10-03 21:54:52 -0500

    Added apiRoutes.js

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friends = [
+    {
+    "name":"Gustav",
+    "photo":"http://www.studiotreasure.com/artists/k/Klimt-images/Gustav-Klimt.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+        ]
+    },
+    {
+        
+    }
+];
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,41 @@
+var friends = require("../data/friends.js")
+console.log(friends);
+
+module.exports = function (app) {
+
+  app.get("/api/friends", function (req, res) {
+    return res.json(friends);
+  });
+
+  app.post("/api/friends", function (req, res) {
+    
+    var user = req.body;
+        console.log(user);
+    user.scores = user.scores.map(function(str){return parseInt(str)})
+
+    var minDiff = 9999;
+    var bestMatch = {};
+
+    // Loop through friends array
+
+    friends.map(function(friend){
+      //loop through friends scores
+      var scoreDiff = 0;
+      friend.scores.map(function(score, i){
+
+        //compare scores to user and keep running total
+        scoreDiff += Math.abs(score - user.scores[i])
+      });
+
+      //if scoreDiff < MinDiff set equal
+      if (minDiff > scoreDiff) {
+        minDiff = scoreDiff;
+        bestMatch = friend;
+      }
+
+    });
+
+    friends.push(user);
+    res.json(bestMatch); //return the just match not all friends
+  });
+}
